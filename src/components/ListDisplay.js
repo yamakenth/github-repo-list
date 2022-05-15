e73a0adcd7f341aa035f6aadb0bee8954aee7530
Ken Yamada <yamakenth@gmail.com>
2022-05-15 17:12:55 +0700

    Show first 100 public repositories

--- src/components/ListDisplay.js
+++ src/components/ListDisplay.js
@@ -1,6 +1,47 @@
+import React, { useEffect, useState } from 'react';
+
+const baseUrl = 'https://api.github.com/repositories';
+
 const ListDisplay = () => {
+  // store repos as state 
+  const [repos, setRepos] = useState([]);
+  
+  // fetch list of repos based on "since" id params
+  const fetchRepos = (since) => {
+    fetch(baseUrl + '?since=' + since, {
+      method: 'GET',
+      headers: { 'Accept': 'application/vnd.github.v3+json' }
+    })
+      .then(res => res.json())
+      .then(data => setRepos(data))
+      .catch(err => console.log('Request Failed', err));  
+  }
+
+  // load first batch of repos on componentDidMount
+  useEffect(() => {
+    fetchRepos(0)
+  }, []);
+  
   return (
-    <h2>ListDisplay</h2>
+    <div>
+      <h2>ListDisplay</h2>
+      <div>
+        {
+          repos.map(repo => {
+            return (
+              <div key={repo.id}>
+                <p>
+                  <strong>ID: </strong>{repo.id}&nbsp;
+                  <strong>Name: </strong><a href={repo.html_url}>{repo.full_name}</a>&nbsp;
+                  <strong>Owner: </strong><a href={repo.owner.html_url}>{repo.owner.login}</a>&nbsp;
+                  <strong>Description: </strong>{repo.description}&nbsp;
+                </p>
+              </div>
+            );
+          })
+        }
+      </div>
+    </div>
   )
 }
 
