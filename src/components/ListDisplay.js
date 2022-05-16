b700489703fb5eaba76608e9515bb03631026968
Ken Yamada <yamakenth@gmail.com>
2022-05-16 12:09:50 +0700

    Store only API URLs of public repos

--- src/components/ListDisplay.js
+++ src/components/ListDisplay.js
@@ -1,25 +1,29 @@
 import React, { useEffect, useState } from 'react';
 
 const baseUrl = 'https://api.github.com/repositories';
+const headers = {
+  method: 'GET',
+  headers: { 'Accept': 'application/vnd.github.v3+json' } //, 'Authorization': 'Basic' + btoa('yamakenth')} // DELETE DELETE DELETE 
+}
 
 const ListDisplay = () => {
   // store repos as state 
   const [repos, setRepos] = useState([]);
   
   // fetch list of repos based on "since" id params
-  const fetchRepos = (since) => {
-    fetch(baseUrl + '?since=' + since, {
-      method: 'GET',
-      headers: { 'Accept': 'application/vnd.github.v3+json' }
-    })
+  const fetchPublicRepos = (url) => {
+    fetch(url, headers)
       .then(res => res.json())
-      .then(data => setRepos(data))
+      .then(data => {
+        const apiUrls = data.map(item => item.url);
+        setRepos(apiUrls);
+      })
       .catch(err => console.log('Request Failed', err));  
   }
 
   // load first batch of repos on componentDidMount
   useEffect(() => {
-    fetchRepos(0)
+    fetchPublicRepos(baseUrl + '?since=0');
   }, []);
   
   return (
@@ -27,18 +31,7 @@ const ListDisplay = () => {
       <h2>ListDisplay</h2>
       <div>
         {
-          repos.map(repo => {
-            return (
-              <div key={repo.id}>
-                <p>
-                  <strong>ID: </strong>{repo.id}&nbsp;
-                  <strong>Name: </strong><a href={repo.html_url}>{repo.full_name}</a>&nbsp;
-                  <strong>Owner: </strong><a href={repo.owner.html_url}>{repo.owner.login}</a>&nbsp;
-                  <strong>Description: </strong>{repo.description}&nbsp;
-                </p>
-              </div>
-            );
-          })
+          JSON.stringify(repos)
         }
       </div>
     </div>
