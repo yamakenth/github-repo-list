ed2bfeb084423413bc61f5c1ebb97547e0384061
Ken Yamada <yamakenth@gmail.com>
2022-05-16 21:56:14 +0700

    Add comments for documentation

--- src/components/PageControls.js
+++ src/components/PageControls.js
@@ -6,29 +6,25 @@ import { fetchPublicRepos } from '../utils/api';
 import { range } from '../utils/utils';
 
 const PageControls = () => {
+  // useContext for repo and curRepo states
   const { repos, setRepos, curRepos, setCurRepos } = useContext(RepoContext);
   
-  // change curRepos onClick
+  // change curRepos onClick on specific page number
   const handlePageClick = (i) => {
-    console.log('clicked on ' + i);
     if (i * 10 > repos.length - 1) {
       fetchPublicRepos(repos[repos.length - 1].id).then(data => setRepos(repos.concat(data)));
     }
     setCurRepos([i * 10, i * 10 + 9]);
   }
 
-  // move to previous page
+  // move to previous page on "< Previous" click
   const handlePrevClick = () => {
-    console.log('clicked on Previous');
-    if (curRepos[0] === 0) {
-      return;
-    }
+    if (curRepos[0] === 0) return;
     setCurRepos([curRepos[0] - 10, curRepos[1] - 10]);
   }
 
-  // move to next page
+  // move to next page on "Next >" click
   const handleNextClick = () => {
-    console.log('clicked on Next');
     if (curRepos[1] + 1 === repos.length) {
       fetchPublicRepos(repos[curRepos[1]].id).then(data => setRepos(repos.concat(data)));
     }
@@ -37,24 +33,42 @@ const PageControls = () => {
   
   return (
     <div className='control-container'>
-      <button className='slide' type='button' onClick={handlePrevClick}><FaChevronLeft />&nbsp;Previous</button>
+      
+      {/* "< Previous" */}
+      <button className='slide' type='button' onClick={handlePrevClick}>
+        <FaChevronLeft />&nbsp;Previous
+      </button>
+
+      {/* "1" when current page num >= 2 */}
       {curRepos[0]> 0 &&
         <button className='page' type='button' onClick={() => handlePageClick(0)}>1</button>
       }
+      {/* "..." next to "1" when curent page num >= 3 */}
       {curRepos[0]> 10 &&
         <span>&middot;&nbsp;&middot;&nbsp;&middot;</span>
       }
+
+      {/* list of page nums, list only 5, "..." at the end */}
       {
-        range(curRepos[0] / 10 + 1, 10).map(n => {
+        range(curRepos[0] / 10 + 1, 5).map(n => {
           return (
-            <button className={`page ${(curRepos[0] / 10 + 1 === n) ? 'active': null}`} key={n} type='button' onClick={() => handlePageClick(n - 1)}>
+            <button 
+              className={`page ${(curRepos[0] / 10 + 1 === n) ? 'active': null}`} 
+              key={n} 
+              type='button' 
+              onClick={() => handlePageClick(n - 1)}
+            >
               {n}
             </button>
           );
         })
       }
       <span>&middot;&nbsp;&middot;&nbsp;&middot;</span>
-      <button className='slide' type='button' onClick={handleNextClick}>Next&nbsp;<FaChevronRight /></button>
+
+      {/* "Next >" */}
+      <button className='slide' type='button' onClick={handleNextClick}>
+        Next&nbsp;<FaChevronRight />
+      </button>
     </div>
   )
 }
