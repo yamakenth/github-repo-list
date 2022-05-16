865c4ae1fe8ace2687a077e30dc022556e8800f4
Ken Yamada <yamakenth@gmail.com>
2022-05-16 14:35:47 +0700

    Move api fetch operations too app.js to pass states as props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import React, { useEffect, useState } from 'react';
+
 import Header from './components/Header';
 import ListDisplay from './components/ListDisplay';
 
+const baseUrl = 'https://api.github.com/repositories';
+const headers = {
+  method: 'GET',
+  headers: { 'Accept': 'application/vnd.github.v3+json' }
+}
+
 const App = () => {
+  // store repos as state 
+  const [repos, setRepos] = useState([]);
+  // store current page of 10 repos *start & end inclusive
+  const [curRepos, setCurRepos] = useState([0, 9])
+  
+  // fetch list of repos based on "since" id params
+  const fetchPublicRepos = (url) => {
+    fetch(url, headers)
+      .then(res => res.json())
+      .then(data => {
+        setRepos(data);
+      })
+      .catch(err => console.log('Request Failed', err));  
+  }
+
+  // load first batch of repos on componentDidMount
+  useEffect(() => {
+    fetchPublicRepos(baseUrl + '?since=0');
+  }, []);
+
   return (
     <div>
       <Header />
-      <ListDisplay />
+      <ListDisplay repos={repos} setRepos={setRepos} curRepos={curRepos} setCurRepos={setCurRepos} />
     </div>
   );
 }
--- src/components/ListDisplay.js
+++ src/components/ListDisplay.js
@@ -1,55 +1,31 @@
-import React, { useEffect, useState } from 'react';
-
-const baseUrl = 'https://api.github.com/repositories';
-const headers = {
-  method: 'GET',
-  headers: { 'Accept': 'application/vnd.github.v3+json' }
-}
-
-const ListDisplay = () => {
-  // store repos as state 
-  const [repos, setRepos] = useState([]);
-  // store current page of 10 repos *start & end inclusive
-  const [curRepos, setCurRepos] = useState([0, 9])
-  
-  // fetch list of repos based on "since" id params
-  const fetchPublicRepos = (url) => {
-    fetch(url, headers)
-      .then(res => res.json())
-      .then(data => {
-        setRepos(data);
-      })
-      .catch(err => console.log('Request Failed', err));  
-  }
-
-  // load first batch of repos on componentDidMount
-  useEffect(() => {
-    fetchPublicRepos(baseUrl + '?since=0');
-  }, []);
-  
+const ListDisplay = (props) => {
   return (
     <div>
       <table>
-        <tr>
-          <th>Repo ID</th>
-          <th>Repo Name</th>
-          <th>Owner ID</th>
-          <th>Owner Name</th>
-          <th>Repo Description</th>
-        </tr>
-        {
-          repos.slice(curRepos[0], curRepos[1] + 1).map(repo => {
-            return (
-              <tr key={repo.id}>
-                <td>{repo.id}</td>
-                <td>{repo.name}</td>
-                <td>{repo.owner.id}</td>
-                <td>{repo.owner.login}</td>
-                <td>{repo.description}</td>
-              </tr>
-            );
-          })
-        }
+        <thead>
+          <tr>
+            <th>Repo ID</th>
+            <th>Repo Name</th>
+            <th>Owner ID</th>
+            <th>Owner Name</th>
+            <th>Repo Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            props.repos.slice(props.curRepos[0], props.curRepos[1] + 1).map(repo => {
+              return (
+                <tr key={repo.id}>
+                  <td>{repo.id}</td>
+                  <td>{repo.name}</td>
+                  <td>{repo.owner.id}</td>
+                  <td>{repo.owner.login}</td>
+                  <td>{repo.description}</td>
+                </tr>
+              );
+            })
+          }
+        </tbody>
       </table>
     </div>
   )
