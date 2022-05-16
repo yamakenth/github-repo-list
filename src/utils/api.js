f1f7ce4f973499e3816037fb57c2f102fa90aca0
Ken Yamada <yamakenth@gmail.com>
2022-05-16 22:00:07 +0700

    Add comments for documentation

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,8 +4,10 @@ const headers = {
   headers: { 'Accept': 'application/vnd.github.v3+json' }
 }
 
+// fetch list of public repos
+// https://docs.github.com/en/rest/repos/repos#list-public-repositories
+// query param of "since" --> only return repos with id greater than this id
 export const fetchPublicRepos = async (queryParam) => {
-  console.log('fetching data...');
   const response = await fetch(`${baseUrl}?since=${queryParam}`, headers);
   const data = await response.json();
   return data;
